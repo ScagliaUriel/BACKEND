4935f43136cb56349cdbaba55e358e851cbc4bc9
Nicetastic <urielscagliarcc@gmail.com>
2023-05-02 07:36:33 -0300

    ProyectoFinal

--- proyectoFinal/ProyectoFinal/src/utils/twilio.js
+++ proyectoFinal/ProyectoFinal/src/utils/twilio.js
@@ -1,5 +1,5 @@
 import twilio from "twilio";
 
-const client = twilio(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 export default client;
