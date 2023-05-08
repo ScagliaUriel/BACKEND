c8d49a488057fc0dcc868925ff8b50bad782bab9
Nicetastic <urielscagliarcc@gmail.com>
2023-05-08 04:42:55 -0300

    Desafio completados

--- desafios/17/routes/info.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getInfo, getInfoSinLog } from "../services/info.js";
-
-const infoRouter =  Router();
-
-infoRouter.get("/infoSinLog", getInfoSinLog);
-infoRouter.get("/info", getInfo);
-
-export default infoRouter
